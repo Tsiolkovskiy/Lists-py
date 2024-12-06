0dd8c1561d9ecf1d93c1e9f995415a22e9e84a45
Tsiolkovskiy <56129993+Tsiolkovskiy@users.noreply.github.com>
2024-12-05 20:47:57 -0500

    Add files via upload

---
+++ lists practice.py
@@ -0,0 +1,20 @@
+spam = [2,5,3.14, 1, -7]
+spam.sort()
+print (spam)
+
+name = 'Zophie the cat'
+print (name[7])
+newName = name[0:7] + 'the' + name[8:12]
+spam = [ 0,1,2,3,4,5]
+cheese = spam
+cheese[1]='Hello!'
+print (cheese)
+print(spam)
+
+def eggs(someParameter):
+    someParameter.append('Hello')
+
+spam=[1,2,3]
+eggs(spam)
+print(spam)
+
